b4aa2f279eb96e7c6ed6120c246e4a273fab97d3
NorwegianWoods <42549462+NorwegianWoods@users.noreply.github.com>
2018-10-18 08:24:46 -0700

    Update Chick.java

--- Chick.java
+++ Chick.java
@@ -1,4 +1,24 @@
-class Chick //implements Animal 
+class Chicken implements Animal 
 {     
-     //your code here
+    private String myType;     
+    private String mySound;      
+    public Chicken(String type, String sound1, String sound2)     {         
+        myType = type;         
+        if (Math.random() < 0.5){
+            mySound = sound1;
+        }     
+        else{
+            mySound = sound2;
+        }
+    }     
+    public Chicken(){         
+        myType = "unknown";         
+        mySound = "unknown";     
+     }      
+    public String getSound(){
+        return mySound;
+    }     
+    public String getType(){
+        return myType;
+    } 
 }
